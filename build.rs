54508e5e4c1ab3c38f49e7497f1df6ef62ffee12
Zhen Zhang <izgzhen@gmail.com>
2016-03-21 15:41:49 +0800

    solve #2, now build script will setup nativefiledialog automatically

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
 use std::env;
+use std::process::Command;
+
 
 fn main() {
-    // Example: NFD_DIR=/PATH/TO/nativefiledialog/src cargo build
-    let nfd_dir = env::var("NFD_DIR").unwrap();
+    let mut p = env::current_dir().unwrap();
 
-    println!("cargo:rustc-link-search=native={}", nfd_dir);
+    // Example: NFD_DIR=/PATH/TO/ cargo build
+    p.push("nativefiledialog");
+    p.push("src");
+
+    Command::new("git").args(&["clone", "https://github.com/mlabbe/nativefiledialog"]).status().unwrap();
+    Command::new("scons").current_dir(&p.as_path()).status().unwrap();
+
+
+    println!("cargo:rustc-link-search=native={}", p.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=nfd");
 }
