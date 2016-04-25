4ea99a2e466fa259da76a50df9ff3a75e4aa89e6
Zhen Zhang <izgzhen@gmail.com>
2016-04-25 08:46:30 +0800

    declare other interfaces; test CString

--- examples/main.rs
+++ examples/main.rs
@@ -1,15 +1,17 @@
 extern crate nfd;
 extern crate libc;
-use libc::{size_t, c_char};
 use std::ptr;
 use nfd::*;
 use std::ffi::CString;
 
 fn main() {
     unsafe {
-        let mut outPath = CString::new("aosidaoweaksnas").unwrap();
-        let result: NFDResult = NFD_OpenDialog(CString::new("png,jpg;pdf").unwrap().as_ptr(), ptr::null(), &mut outPath.into_raw());
+        // let mut out_path = CString::new("").unwrap();
+        let out_path: *mut (*mut NFDChar) = &mut ptr::null_mut();
+        let result: NFDResult = NFD_OpenDialog(CString::new("png,jpg;pdf").unwrap().as_ptr(), ptr::null(), out_path);
 
+        let cstring = CString::from_raw(*out_path);
         println!("Result: {:?}", result);
+        println!("cstring: {:?}", cstring);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ extern crate cocoa;
 
 use libc::{size_t, c_char};
 
+pub type NFDChar = c_char;
+
 #[repr(C)]
 pub struct NFDPathSet {
     buf: *mut c_char,
@@ -27,7 +29,32 @@ pub enum NFDResult {
 
 #[link(name = "nfd", kind = "static")]
 extern {
-    pub fn NFD_OpenDialog(filterList: *const c_char,
-                          defaultPath: *const c_char,
-                          outPath: *mut (*mut c_char)) -> NFDResult;
+    /* single file open dialog */
+    pub fn NFD_OpenDialog(filterList: *const NFDChar,
+                          defaultPath: *const NFDChar,
+                          outPath: *mut (*mut NFDChar)) -> NFDResult;
+
+    /* multiple file open dialog */
+    pub fn NFD_OpenDialogMultiple(filterList: *const NFDChar,
+                                  defaultPath: *const NFDChar,
+                                  outPaths: *mut NFDPathSet) -> NFDResult;
+
+    /* save dialog */
+    pub fn NFD_SaveDialog(filterList: *const NFDChar,
+                          defaultPath: *const NFDChar,
+                          outPath: *mut (*mut NFDChar)) -> NFDResult;
+
+    /* get last error -- set when nfdresult_t returns NFD_ERROR */
+    pub fn NFD_GetError() -> *const NFDChar;
+
+    /* get the number of entries stored in pathSet */
+    pub fn NFD_PathSet_GetCount(pathSet: *const NFDPathSet) -> size_t;
+
+
+    /* Get the UTF-8 path at offset index */
+    pub fn NFD_PathSet_GetPath(pathSet: *const NFDPathSet,
+                               index: size_t) -> *mut NFDChar;
+
+    /* Free the pathSet */
+    pub fn NFD_PathSet_Free(pathSet: *mut NFDPathSet);
 }
