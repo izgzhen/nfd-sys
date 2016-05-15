5c4e72e8c7a46bcca36da24eda50ed79fd3901d8
Zhen Zhang <izgzhen@gmail.com>
2016-05-15 17:56:24 +0800

    add source into tree; update README; bump version

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ See [libnfd](https://github.com/izgzhen/libnfd) for high-level bindings.
 ## Supported platforms
 * [x] OS X (10.11)
 * [x] Linux x86_64 (Ubuntu 15.04, 14.04)
-* [ ] Linux i386
+* [x] Linux i386
 * [ ] Windows
 
 ## Dependencies
--- build.rs
+++ build.rs
@@ -7,7 +7,6 @@ fn main() {
     p.push("nativefiledialog");
     p.push("src");
 
-    Command::new("git").args(&["clone", "https://github.com/mlabbe/nativefiledialog"]).status().unwrap();
     Command::new("scons").env("CFLAGS", "-fPIC").current_dir(&p.as_path()).status().unwrap();
 
     println!("cargo:rustc-link-search=native={}", p.to_str().unwrap());
